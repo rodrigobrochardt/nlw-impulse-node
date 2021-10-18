58c5c1ab0047e5415615550c85f36fdadda4dac2
rodrigobrochardt <rodrigobrochardt@gmail.com>
2021-10-18 19:55:13 -0300

    init

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import "dotenv/config"
+import express from "express";
+
+const app = express();
+
+
+app.get("/github",(request,response)=>{
+  response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`);
+});
+app.listen(4000,() => console.log(`Server is running on port 4000`));
