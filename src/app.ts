bfb00314d13f1c29ba3c3e3793e8b5646890a67b
rodrigobrochardt <rodrigobrochardt@gmail.com>
2021-10-19 12:21:11 -0300

    buscas no banco e auth

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,35 @@
-import "dotenv/config"
+import "dotenv/config";
 import express from "express";
+import cors from "cors";
+import { router } from "./routes";
+import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
+app.use(cors());
 
-
-app.get("/github",(request,response)=>{
-  response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`);
+const serverHttp = http.createServer(app);
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*",
+  },
 });
-app.listen(4000,() => console.log(`Server is running on port 4000`));
+
+io.on("connection", (socket) => {
+  console.log(`Usuario conectado no socket ${socket.id}`);
+});
+app.use(express.json());
+
+app.use(router);
+app.get("/github", (request, response) => {
+  response.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+  );
+});
+
+app.get("/signin/callback", (request, response) => {
+  const { code } = request.query;
+  return response.json(code);
+});
+
+export { serverHttp, io };
